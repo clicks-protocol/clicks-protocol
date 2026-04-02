e54522850e2a6abd26b5eba0b25f174fb0f2d93e
clicksprotocol <hello@clicksprotocol.xyz>
2026-04-02 14:58:18 +0200

    Defer X pixel script for faster page rendering

--- landing-v3/app/layout.tsx
+++ landing-v3/app/layout.tsx
@@ -140,6 +140,18 @@ export default function RootLayout({
             }),
           }}
         />
+        {/* X (Twitter) Pixel - deferred to not block rendering */}
+        <script
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','rbppe');
+`,
+          }}
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
